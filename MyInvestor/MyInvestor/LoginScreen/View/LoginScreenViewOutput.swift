260cbd22e07429f410efe30179ec09bf041ba1d8
cosmonavts_futeresssss <102899513+namerr1@users.noreply.github.com>
2025-12-08 22:44:38 +0300

    Delete MyInvestor directory

--- MyInvestor/MyInvestor/LoginScreen/View/LoginScreenViewOutput.swift
+++
@@ -1,9 +0,0 @@
-protocol LoginScreenViewOutput: AnyObject {
-    var state: LoginScreenViewState? { get set }
-    func login()
-    func handleForgotPassword()
-    func handleAppleLogin()
-    func handleFacebookLogin()
-    func handleGoogleLogin()
-    func handleSignUp()
-}
--- MyInvestor/MyInvestor/Services/AuthServiceProtocol.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
- 
-protocol AuthServiceProtocol {
-    func login(email: String, password: String, completion: @escaping (Result<String, AuthError>) -> Void)
-    func saveSession(email: String)
-    func logout()
-    func hasActiveSession() -> Bool
-    func getSavedEmail() -> String?
-}
