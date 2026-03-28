ab96b4abde9edcfbabaae8c5333770bffd46026f
ElvinCool <2705ie@gmail.com>
2026-03-28 15:41:18 +0300

    Initial Commit

---
+++ MyInvestor/MyInvestorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyInvestorApp.swift
+//  MyInvestor
+//
+//  Created by Ilya Ermakov on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MyInvestorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
